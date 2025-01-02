90cf12a6a8bdcdc5e94c45feb8ee15e8686a0765
Joao <jovmdcomenezes@gmail.com>
2025-01-02 13:50:53 -0300

    acl migrations

--- database/migrations/2024_12_31_192833_create_ability_role_table.php
+++ database/migrations/2024_12_31_192833_create_ability_role_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('ability_role', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('ability_id');
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('CASCADE');
+            $table->foreign('ability_id')->references('id')->on('abilities')->onDelete('CASCADE');
         });
     }
 
